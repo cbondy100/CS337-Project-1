f0123c35b3948c317ed858523316cf32645bc899
Callum Bondy <callum.bondy@gmail.com>
2023-01-24 15:37:04 -0600

    re work

--- regexPlay.py
+++ regexPlay.py
@@ -6,11 +6,27 @@ import re
 # "[And the] Award [for Best Actor] goes to [Brad Pitt]"
 # "[Brad Pitt is the] winner [of Best Actor]"
 
-tweets = ["Brad Pitt wins Best Actor", "Micheal Bay wins best director", "Brad Pitt is such a stupid guy", "Callum Bondy wins Best Coder"]
+tweets = ["Brad Pitt wins Best Actor", "Micheal Bay wins best director", "Brad Pitt is such a stupid guy", "And the Award for best actor goes to Brad Pitt"]
 
-string = "Brad Pitt is the Best Actor"
-results = re.search(r'[^a-zA-Z]wins[^a-zA-Z]', string)
-print(results.group())
+regular_expression_list = [r'[^a-zA-Z]wins[^a-zA-Z]', r'[^a-zA-Z][aA]ward[^a-zA-Z]goes to[^a-zA-Z]']
+
+
+kept_tweets = []
+for tweet in tweets:
+    for reg in regular_expression_list:
+        result = re.search(reg, tweet)
+        if result != None:
+            #this means we have passed one of our regular expressions so we keep this tweet
+            kept_tweets.append(tweet)
+            break
+        else:
+            print("Tweet thrown out")
+
+print(kept_tweets)
+
+#string = "Brad Pitt wins Best Actor"
+#results = re.search(r'[^a-zA-Z]wins[^a-zA-Z]', string)
+#print(results.group())
 
 #Start with a tweet
 #check it against our family of regular expressions
