a96f4295f84c0680da11916471b9334b22164bd9
Callum Bondy <callum.bondy@gmail.com>
2023-02-04 19:26:29 -0600

    nominee work

--- nominees.py
+++ nominees.py
@@ -4,6 +4,7 @@ import json
 import nltk
 import re
 import en_core_web_sm
+import jellyfish
 
 #nlp = en_core_web_sm.load()
 
@@ -69,27 +70,12 @@ def get_nom_tweets(tweets, award_list):
 
     nom_tweets = []
     for tweet in tweets:
-        tweet_text = tweet["text"]
+        tweet_text = removePunc(tweet["text"])
         if tweetFilter(tweet_text, regex) and not tweetFilter(tweet_text, regex_blacklist):
             #this means we matched out regex and did not match our blacklist
             for award_name in award_list:
                 if award_name in tweet_text.lower():
                     nom_tweets.append(tweet_text)
-    return nom_tweets
-
-    '''
-    print("Length Before: " + str(len(nom_tweets)))
-    for tweet in nom_tweets:
-        print(tweet)
-        tweet_low = tweet.lower()
-        # loop through possible names for individual award
-        for award_name in award_list:
-            # remove tweet from list if there is no award name match
-            if award_name not in tweet_low:
-                print(tweet)
-                nom_tweets.remove(tweet)
-    print("Length After: " + str(len(nom_tweets)))
-    '''
     return nom_tweets
     
 
@@ -104,7 +90,6 @@ def checkNames(tweet):
     tagged = nltk.pos_tag(tokens)
     entities = nltk.ne_chunk(tagged)
 
-    #print(entities)
     # loop through chunks to find the person    
     for chunk in entities:
         #only look at the trees
@@ -119,16 +104,44 @@ def checkNames(tweet):
     return name_list
 
 # function to create tweet dictionary (lets just get some names)
-def buildTweetDict(tweet_set):
+def buildNameList(tweet_set):
     full_name_list = []
-    
     for tweet in tweet_set:
+        #print(tweet)
         # check if the name list is empty
         full_name_list += checkNames(tweet)
-        print("DONE WITH ITER")
 
-    print(full_name_list)
+    #print(full_name_list)
     return full_name_list
+
+def countNames(name_list):
+    #print(name_list)
+    full_count_dict = {}
+
+    #put first element into count list
+
+    #print(name_list)
+    # checks each name against our list of names
+    
+    for name in name_list:
+        full_count_dict[name] = full_count_dict.get(name, 0) + 1
+    '''
+    for element in full_count_list:
+        for name in name_list:
+            print(element[0] + name)
+            if name == element[0]:
+                print("FOUND A MATCH: " + name)
+                element[1] += 1
+                print(full_count_list)
+                
+            else:
+                print("ADD A NEW NOMINEE: " + name)
+                full_count_list.append([name, 1])
+                #name_list.remove(name)
+                print(full_count_list)
+    '''    
+
+    print(full_count_dict)
 
 def testForAward(tweets):
     # dif names for same award (hardcoded)
@@ -136,19 +149,29 @@ def testForAward(tweets):
 
 if __name__ == '__main__': 
 
-    award_name_list = ["best actress drama", "best drama actress", "motion picture drama", "best actress"]
+    award_name_list = ["best actor", "best actor drama", "best actor motion picture drama", "actor drama", "drama actor", "award"]
+    # From this awards list we would like to extract:
+    # Jessica Chastain, Marion Cotillard, Helen Mirren, Naomi Watts, Rachel Weisz
+
+    #@glasneronfilm @ZulekhaNathoo Best Actress-Drama, I'm calling Naomi Watts.  Best Actress-Musical/Comedy, Jennifer Lawrence. #goldenglobes
+    #RETWEET if you think she deserves to win for her performance in #TheImpossible.
+    #"THE D IS SILENT" - nominee Marion Cotillard, seconds before flipping a table at the #goldenglobes
 
     tweet_data = loadjson()
     nom_tweets = get_nom_tweets(tweet_data, award_name_list)
     #print(nom_tweets)
-    nom_list = buildTweetDict(nom_tweets)
-    #print(nom_list)
-    #print(tweet_dict)
-    #print(r)
+    name_list = buildNameList(nom_tweets)
+    countNames(name_list)
+    
+    print(jellyfish.jaro_distance(u"Lincoln", u"LincolnGoldenGlobes"))
+    print(jellyfish.jaro_distance(u"Lincoln", u"LincolnMakes"))
+    print(jellyfish.jaro_distance(u"Lincoln", u"Wow"))
+    print(jellyfish.jaro_distance(u"Lincoln", u"Argo"))
 
-
-
-    #tweet = "Brad Pitt wins best actor!67"
-    #s = tokenize_preprocess(tweet)
-    #s2 = removePunc(tweet)
-    #print(s2)
+    '''
+    for tweet in tweet_data:
+       tweet_text = tweet['text']
+       if tweetFilter(tweet_text, r"([Jj]oaquin)"):
+        print(tweet_text)
+    '''
+    
